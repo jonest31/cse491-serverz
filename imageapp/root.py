17c391e8930f1ee4245d9407073e8afc8cf02e8e
Taylor Jones <jonest31@arctic.cse.msu.edu>
2014-03-26 17:49:45 -0400

    hw9 and project option #3

---
+++ imageapp/root.py
@@ -0,0 +1,70 @@
+import quixote
+from quixote.directory import Directory, export, subdir
+
+from . import html, image
+
+class RootDirectory(Directory):
+    _q_exports = []
+
+    @export(name='') # this makes it public.
+    def index(self):
+        return html.render('index.html')
+
+    @export(name='upload')
+    def upload(self):
+        return html.render('upload.html')
+
+    @export(name='upload_receive')
+    def upload_receive(self):
+        request = quixote.get_request()
+        print request.form.keys()
+
+        the_file = request.form['file']
+        print dir(the_file)
+        print 'received file with name:', the_file.base_filename
+        data = the_file.read(int(1e9))
+
+        image.add_image(data)
+
+        return quixote.redirect('./')
+
+    @export(name='image')
+    def image(self):
+        return html.render('image.html')
+
+    @export(name='image_raw')
+    def image_raw(self):
+        response = quixote.get_response()
+        response.set_content_type('image/png')
+        img = image.get_latest_image()
+        return img
+
+    @export(name='body.jpg')
+    def body_jpg(self):
+        data = html.get_image('body.jpg')
+        return data
+
+    @export(name='content.jpg')
+    def content_jpg(self):
+        data = html.get_image('content.jpg')
+        return data
+
+    @export(name='footer.gif')
+    def footer_gif(self):
+        data = html.get_image('footer.gif')
+        return data
+
+    @export(name='header.jpg')
+    def header_jpg(self):
+        data = html.get_image('header.jpg')
+        return data
+
+    @export(name='menubottom.jpg')
+    def menubottom_jpg(self):
+        data = html.get_image('menubottom.jpg')
+        return data
+
+    @export(name='menuhover.gif')
+    def menuhover_gif(self):
+        data = html.get_image('menuhover.gif')
+        return data
--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 import argparse
 from app import make_app
 import imageapp
+import os
 import quixote
 from quixote.demo.altdemo import create_publisher
 import random
@@ -22,7 +23,7 @@ def handle_connection(conn, port):
     count = 0
     env = {}
     while request[-4:] != '\r\n\r\n':
-        request += conn.recv(1)
+         request += conn.recv(1)
 
     request, data = request.split('\r\n',1)
     headers = {}
@@ -45,7 +46,7 @@ def handle_connection(conn, port):
     env['wsgi.multiprocess'] = False
     env['wsgi.run_once'] = False
     env['wsgi.url_scheme'] = 'http'
-    #env['HTTP_COOKIE'] = headers['cookie']
+    env['HTTP_COOKIE'] = headers['cookie'] if 'cookie' in headers.keys() else ''
 
     body = ''
     if request.startswith('POST '):
@@ -73,7 +74,7 @@ def handle_connection(conn, port):
     conn.close()
 
 def get_args():
-    app_list = ['altdemo', 'image', 'myapp']
+    app_list = ['altdemo', 'image', 'myapp', 'quotes', 'chat']
     parser = argparse.ArgumentParser()
     parser.add_argument('-A', action="store",
                               dest='arg_app',
@@ -141,5 +142,12 @@ def main(socketmodule=None):
         print 'The Web server URL for this would be http://%s:%d/' % (host, port)
         httpd.serve_forever()
 
+    elif app in ('quotes', 'chat'):
+        if port == 0:
+            port = random.randint(8000, 9999)
+        os.chdir(app)
+        os.system("python2.7 %s-server %d" % (app, port))
+
 if __name__ == '__main__':
     main()
+
